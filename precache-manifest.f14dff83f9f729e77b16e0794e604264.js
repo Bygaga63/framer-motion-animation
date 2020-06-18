4093472fc3245468e2baf3943fa1edf1069c1b50
dmitry.mukhin <dmitry.mukhin@citronium.com>
2020-06-18 08:50:47 +0300

    Updates

---
+++ precache-manifest.f14dff83f9f729e77b16e0794e604264.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49f5d357488ddbd58faefc6879477081",
+    "url": "/Bygaga63/framer-motion-animation/index.html"
+  },
+  {
+    "revision": "12590488b8f86055619d",
+    "url": "/Bygaga63/framer-motion-animation/static/css/main.86dbead9.chunk.css"
+  },
+  {
+    "revision": "f7b952021e7c01d31736",
+    "url": "/Bygaga63/framer-motion-animation/static/js/2.bee08571.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/Bygaga63/framer-motion-animation/static/js/2.bee08571.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12590488b8f86055619d",
+    "url": "/Bygaga63/framer-motion-animation/static/js/main.868b05fa.chunk.js"
+  },
+  {
+    "revision": "a2a0c93a027bbd234382",
+    "url": "/Bygaga63/framer-motion-animation/static/js/runtime-main.785ce22e.js"
+  }
+]);
